5c22914056c5054d261c16bb84f12d77e1c538af
Harsh Raj <harshraj9988@gmail.com>
2023-03-07 19:57:27 +0530

    7-mar-23

--- Arrays/setMatrixZeroes.py
+++ Arrays/setMatrixZeroes.py
@@ -8,33 +8,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m, n = len(matrix), len(matrix[0])
-        first_element_zero = matrix[0][0] == 0
-        first_row_has_zero = 0 in [matrix[0][i] for i in range(1, n)]
-        first_column_has_zero = 0 in [matrix[i][0] for i in range(1, m)]
+        m, n, first_col = len(matrix), len(matrix[0]), True
 
-        for i in range(1, m):
+        for i in range(m):
+            if matrix[i][0] == 0:
+                first_col = False
             for j in range(1, n):
                 if matrix[i][j] == 0:
-                    matrix[0][j] = 0
-                    matrix[i][0] = 0
+                    matrix[0][j] = matrix[i][0] = 0
 
-        for i in range(1, m):
-            if matrix[i][0] == 0:
-                for j in range(1, n):
-                    matrix[i][j] = 0
-        for j in range(1, n):
-            if matrix[0][j] == 0:
-                for i in range(1, m):
-                    matrix[i][j] = 0
-        if first_element_zero:
-            for i in range(1, m):
-                matrix[i][0] = 0
+        for i in range(m-1, -1, -1):
             for j in range(1, n):
-                matrix[0][j] = 0
-        if first_row_has_zero:
-            for j in range(0, n):
-                matrix[0][j] = 0
-        if first_column_has_zero:
-            for i in range(0, m):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+            if not first_col:
                 matrix[i][0] = 0
